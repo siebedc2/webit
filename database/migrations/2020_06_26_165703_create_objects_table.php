cfb336c7e185790b878ec3b160dd126fdb9b6ab6
Siebe <siebedc2@gmail.com>
2020-06-26 19:05:12 +0200

    Database setup

---
+++ database/migrations/2020_06_26_165703_create_objects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateObjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('objects', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug');
+            $table->text('description');
+            $table->text('pictures')->nullable();
+            $table->enum('status', ['online', 'offline'])->default('online');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('objects');
+    }
+}
---
+++ database/migrations/2020_06_26_165901_create_bids_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBidsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bids', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('object_id');            
+            $table->string('price'); 
+            $table->timestamps(); 
+            
+            // indexes
+            $table->index(['user_id']);
+            $table->index(['object_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bids');
+    }
+}
